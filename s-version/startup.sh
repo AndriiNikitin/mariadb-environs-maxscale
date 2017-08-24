b4eed7c203c4a536caa4169bd5c62cb769e70113
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-08-24 15:49:27 +0200

    typo

--- s-version/startup.sh
+++ s-version/startup.sh
@@ -5,5 +5,5 @@ mkdir -p __workdir/datadir
 mkdir -p __workdir/cache
 mkdir -p __workdir/maxscale.cnf.d
 
-__workdir/../_depot/s-tar/__version/bin/maxscale -f __workdir/s.cnf -L __workdir/log -D __workdir/datadir --piddir=__workdir --syslog=no --libdir=__workdir/../_depot/s-tar/2.1.6/lib/maxscale --cachedir=__workdir/cache --persistdir=__workdir/maxscale.cnf.d
+__workdir/../_depot/s-tar/__version/bin/maxscale -f __workdir/s.cnf -L __workdir/log -D __workdir/datadir --piddir=__workdir --syslog=no --libdir=__workdir/../_depot/s-tar/__version/lib/maxscale --cachedir=__workdir/cache --persistdir=__workdir/maxscale.cnf.d
 
