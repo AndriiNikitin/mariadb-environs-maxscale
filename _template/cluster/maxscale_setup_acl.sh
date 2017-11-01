45994906431c04d18da32ee1f496df6fe6d84ffb
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-11-01 10:30:26 +0100

    start server if needed to setup maxscale acl

--- _template/cluster/maxscale_setup_acl.sh
+++ _template/cluster/maxscale_setup_acl.sh
@@ -4,9 +4,17 @@ set -e
 
 # currently works only on localhost
 
+
+
 for eid in $(cat __clusterdir/nodes.lst) ; do
-  __clusterdir/../$eid*/sql.sh create user if not exists "maxscale"@"127.0.0.1" identified by '"maxscale"'
-  __clusterdir/../$eid*/sql.sh grant all on \*.\* to maxscale@127.0.0.1
-  __clusterdir/../$eid*/sql.sh create user if not exists "maxscale"@"localhost" identified by '"maxscale"'
-  __clusterdir/../$eid*/sql.sh grant all on \*.\* to maxscale@localhost
+  was_started=no
+  __clusterdir/../$eid*/status.sh &> /dev/null || { __clusterdir/../$eid*/startup.sh && was_started=yes; }
+  
+  { __clusterdir/../$eid*/sql.sh create user /*M!100100 if not exists*/ "maxscale"@"127.0.0.1" identified by '"maxscale"' && \
+     __clusterdir/../$eid*/sql.sh grant all on \*.\* to maxscale@127.0.0.1; } || :
+
+  {  __clusterdir/../$eid*/sql.sh create user /*M!100100 if not exists*/ "maxscale"@"localhost" identified by '"maxscale"' && \
+      __clusterdir/../$eid*/sql.sh grant all on \*.\* to maxscale@localhost; } || :
+
+  [ "$was_started" == no ] || __clusterdir/../$eid*/shutdown.sh
 done
